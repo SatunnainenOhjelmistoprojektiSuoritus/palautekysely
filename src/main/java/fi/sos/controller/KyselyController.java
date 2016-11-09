9a3fdc5704c66b05c81a908add34080c0817e93a
Antti Ryökkynen <antti.ryokkynen@myy.haaga-helia.fi>
2016-11-09 11:20:36 +0200

    Test

--- src/main/java/fi/sos/controller/KyselyController.java
+++ src/main/java/fi/sos/controller/KyselyController.java
@@ -9,17 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.sos.bean.Kysely;
+import fi.sos.bean.Vastaus;
 import fi.sos.dao.KyselyDAO;
+import fi.sos.dao.VastausDAO;
 
 
 @Controller
 public class KyselyController {
 	@Inject
 	KyselyDAO dao;
+	
+	@Inject 
+	VastausDAO vdao;
 
 	@RequestMapping("kyselyt.json")
 	public @ResponseBody List<Kysely> haeKaikkiKyselytJSON() {
-		List<Kysely> kyselyt = dao.haeKaikki();
+		List<Kysely> kyselyt = dao.haeKysymys(2);
+
 		return kyselyt;
 	}
 
--- src/main/java/fi/sos/dao/KyselyRowMapper.java
+++ src/main/java/fi/sos/dao/KyselyRowMapper.java
@@ -2,10 +2,12 @@ package fi.sos.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import fi.sos.bean.Kysely;
+import fi.sos.bean.Vastaus;
 
 
 public class KyselyRowMapper implements RowMapper<Kysely> {
@@ -13,11 +15,15 @@ public class KyselyRowMapper implements RowMapper<Kysely> {
 	public Kysely mapRow(ResultSet rs, int rowNum) throws SQLException {
 		
 		Kysely k = new Kysely();
+		
 		k.setId(rs.getInt("id"));
-		k.setKysymys(rs.getString("kysymys"));
-		k.setMonivastaus(rs.getInt("monivastaus"));
-		k.setAvoin(rs.getString("avoin"));
-						
+		k.setKysely_id(rs.getInt("kysely_id"));
+		k.setVastaus_id(rs.getInt("vastaus_id"));
+		k.setKysely_nimi(rs.getString("kysely_nimi"));
+		k.setKysely_kysymys(rs.getString("kysely_kysymys"));
+		k.setVastaus(rs.getString("vastaus"));
+		k.setVastaus_tyyppi(rs.getString("vastaus_tyyppi"));		
+		
 		return k;
 	}
 
